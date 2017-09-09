24fb6cbe87d4f8efaeb3f4c7115a350cc58242a5
SpadesCT <31596430+SpadesCT@users.noreply.github.com>
2017-09-08 20:39:48 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
 const Discord = require('Discord.js');
-const prefix = "!"
-var bot = new Discord.Client();
+const TOKEN = "censored";
+const prefix = "/"
+var bot = new Discord.Client()
+
 bot.on('ready',() => {
-	console.log('Ready');
+	console.log('>>> I am Online');
+bot.user.setGame('/help');
 });
-bot.on('message', message => {
+bot.on('message', async message => {
 if(message.author.bot) return;
 
-if (message.content.startsWith(prefix + 'ping')) {
-  message.reply("Pong!");
-};
+if (message.content.startsWith(prefix + 'userinfo')) {
+	let user = message.mentions.users.first() || message.author;
+	let embed = new Discord.RichEmbed()
 	
-if (message.content.startsWith(prefix + 'test')) {
-  message.channel.send("Test");
+	.setAuthor(message.author.username)
+	.setDescription("Users Info")
+	.setColor("#428cdf")
+	.addField("Username", `${message.author.username}#${message.author.discriminator}`)
+	.addField("Nickname", `${message.member.nickname || "None"}`)
+	.addField("UserID", message.author.id)
+	.addField("AvatarURL", message.author.avatarURL)
+	.addField("Playing", user.presence.game ? user.presence.game.name : 'None');
+
+	message.channel.sendEmbed(embed);
 };
+
+if (message.content.startsWith(prefix + 'userperms')) {
+	message.channel.send('`User Permissions:\n`' +
+		JSON.stringify(message.channel.permissionsFor(message.author).serialize(), null, 4));
+};
+
+if (message.content.startsWith(prefix + 'setname')) {
+	message.channel.setName(message.content.substr(8));
+};
+const msg = await message.channel.send('Ping!');
+      msg.edit(`Pong! (Response time: ${msg.createdTimestamp - message.createdTimestamp}ms)`);
 });
 bot.login(TOKEN);
